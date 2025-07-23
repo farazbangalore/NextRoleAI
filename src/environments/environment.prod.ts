5ff312ad58049e79a07054df2252ee3cd4c1623a
Faraz Hussain <farazhussain5554@gmail.com>
2025-07-23 14:07:33 +0530

    Added supbase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 // environment.prod.ts (for production)
 export const environment = {
   production: true,
-  BASE_URL: 'https://nextrole-1aba.onrender.com'
+  BASE_URL: 'https://nextrole-1aba.onrender.com',
+  supabaseUrl: 'https://kqmbyobywpvdmsgofyte.supabase.co',
+  supabaseKey: 'sb_secret_F3jbyALar4ib7gRtREh1uQ_qofN1TJz',
 };
